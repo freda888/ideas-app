0435663fa6acb45d017831b6ee7d193d83e3386d
P Kelly <pkkelly2020@gmail.com>
2023-04-27 10:04:06 -0600

    Initial commit with get and post routing

---
+++ routes/ideas.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const router = express.Router()
+
+const ideas = [
+  {
+    id: 1,
+    text: 'Positive NewsLetter, a newsletter that only shares positive, uplifting news',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'Oat milk cartons that turn a different color the older that your oat milk is getting',
+    tag: 'Inventions',
+    username: 'SteveRogers',
+    date: '2022-01-02',
+  },
+  {
+    id: 3,
+    text: 'ATM location app which lets you know where the closest ATM is and if it is in service',
+    tag: 'Software',
+    username: 'BruceBanner',
+    date: '2022-01-02',
+  },
+]
+
+// get all ideas
+router.get('/', (req, res) => {
+  res.json({ success: true, data: ideas })
+})
+
+// get one idea by id
+router.get('/:id', (req, res) => {
+  const idea = ideas.find(idea => idea.id === +req.params.id)
+
+  if (!idea) {
+    return res.status(404).json({ success: false, error: 'Not found' })
+  }
+
+  res.json({ success: true, data: idea })
+})
+
+// add idea
+router.post('/', (req, res) => {
+  const idea = {
+    id: ideas.length + 1,
+    text: req.body.text,
+    tag: req.body.tag,
+    user: req.body.username,
+    date: new Date().toISOString().slice(0, 10),
+  }
+
+  ideas.push(idea)
+
+  res.json({ success: true, data: idea })
+})
+
+module.exports = router
